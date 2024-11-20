3ca9997c4b658854b852b24f5bc3a527c69f4290
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-20 18:00:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  methods:{
+        getData(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+          .then(response => {
+          this.projects = response.data.response;
+          console.log(response.data.response);
+          })
+          .catch(function (error) {
+          // handle error
+          console.log(error);
+          })
+          .finally(function () {
+          // always executed
+          })
+      },
+  },
+  created(){
+    this.getData();
+
+  }
+}
+</script>
+
+<template>
+<h1>Helloooooo</h1>
+<div class="div" v-for="project in projects">
+  <h1>{{ project.name }}</h1>
+</div>
+</template>
+
+<style >
+</style>
